2c2a64cd85d23e3223ebab30cd4775786b235ab9
Andrew Herculano <andrewherculano@hotmail.com>
2021-01-08 10:24:28 -0300

    Add routes and stepsheader

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -10,13 +10,13 @@ function Footer() {
     <footer className="main-footer">
       App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
       <div className="footer-icons">
-        <a href="https://www.youtube.com" target="_blank">
+        <a href="https://www.youtube.com" target="_new">
             <YoutubeIcon />
         </a>
-        <a href="https://www.linkedin.com" target="_blank">
+        <a href="https://www.linkedin.com" target="_new">
             <LinkedinIcon />
         </a>
-        <a href="https://www.instagram.com" target="_blank">
+        <a href="https://www.instagram.com" target="_new">
             <InstagramIcon />
         </a>
       </div>
