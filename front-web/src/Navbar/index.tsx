a464fb3a77575d867b0dc73089604655c1fc4656
Andrew Herculano <andrewherculano@hotmail.com>
2021-01-09 11:03:57 -0300

    Add env vars

--- front-web/src/Navbar/index.tsx
+++ front-web/src/Navbar/index.tsx
@@ -7,7 +7,7 @@ function Navbar(){
     return (
         <nav className="main-navbar">
             <Logo />
-            <Link to="/" className="logo-text">DS Delivery</Link>
+            <Link to="/" className="logo-text">DS Delivery ANDREW</Link>
         </nav>
     )
 }
